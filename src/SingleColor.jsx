72da49264339d4727975ec538a27cdb790304282
Xievy <xuhvm3@gmail.com>
2024-10-15 12:05:34 +0530

    done

--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,8 +1,20 @@
+import { toast } from "react-toastify";
+
 const SingleColor = ({index, color}) => {
     const {hex, weight } =color;
+    const saveTo = async () => {
+        if(navigator.clipboard){
+            try {
+                await navigator.clipboard.writeText(`#${hex}`)
+                toast.success('Color copied to clipboard')
+            } catch (error) {
+                toast.error('Failed to copy')
+            }
+        }
+    }
 
   return (
-    <article style={{background: `#${hex}`}}>
+    <article style={{background: `#${hex}`}} onClick={saveTo}>
         <p>{weight}%</p>
         <p>#{hex}</p>
     </article>
